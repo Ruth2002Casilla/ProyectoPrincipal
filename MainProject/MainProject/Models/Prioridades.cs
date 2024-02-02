068745db3015707acfd2e420a851f0dfade32df6
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-02 09:23:31 -0400

    Creando la carpeta Models y su clase Prioridades con los cambios especificados por el Maestro.

--- MainProject/MainProject/Models/Prioridades.cs
+++ MainProject/MainProject/Models/Prioridades.cs
@@ -1,7 +1,18 @@
-﻿namespace MainProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainProject.Models
 {
     public class Prioridades
     {
+        [Key]
 
+        [Required(ErrorMessage = "Este campo es Obligatorio")]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es Obligatorio")]
+        public string Descripcion { get; set; }
+
+        [Range(1, 31, ErrorMessage = "Valores validos del 1 al 31")]
+        public int DiasCompromiso { get; set; }
     }
 }
