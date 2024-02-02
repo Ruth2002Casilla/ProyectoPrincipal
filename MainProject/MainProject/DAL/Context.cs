3e5ead57210bd76d444a590ae2d46c6fd68ac4e0
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-02 09:28:36 -0400

    Creando el contexto para la conexion a la base de datos.

---
+++ MainProject/MainProject/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MainProject.Models;
+
+namespace MainProject.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Prioridades> Prioridades { get; set; }
+    }
+}
